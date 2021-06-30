8b72ebbef955272c0826d8ee18734c593b3e6471
Modu <35815941+hmodu@users.noreply.github.com>
2021-06-30 10:47:11 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,72 @@
+let products = [
+    {
+        "image": "Client/images/file.jpg",
+        "subject": "Physics",
+        "price": 50,
+        "location": "Geneva",
+        "availableInventory": 5
+    },
+    {
+        "image": "che.png",
+        "subject": "Chemistry",
+        "price": 35,
+        "location": "Lagos",
+        "availableInventory": 5
+    },
+    {
+        "image": "bio.jpg",
+        "subject": "Biology",
+        "price": 38,
+        "location": "Abuja",
+        "availableInventory": 5
+    },
+    {
+        "image": "psy.jpg",
+        "subject": "Psychology",
+        "price": 39,
+        "location": "Ontario",
+        "availableInventory": 5
+    },
+    {
+        "image": "geo.png",
+        "subject": "Geography",
+        "price": 42,
+        "location": "Granada",
+        "availableInventory": 5
+    },
+    {
+        "image": "zoo.jpg",
+        "subject": "Zoology",
+        "price": 49,
+        "location": "Conackry",
+        "availableInventory": 5
+    },
+    {
+        "image": "eng.jpg",
+        "subject": "Engineering",
+        "price": 38,
+        "location": "Munich",
+        "availableInventory": 5
+    },
+    {
+        "image": "com.jpg",
+        "subject": "Computer",
+        "price": 41,
+        "location": "Timbuktu",
+        "availableInventory": 5
+    },
+    {
+        "image": "mat.jpg",
+        "subject": "Mathematics",
+        "price": 47,
+        "location": "Abuja",
+        "availableInventory": 5
+    },
+    {
+        "image": "soc.png",
+        "subject": "Sociology",
+        "price": 26,
+        "location": "Toronto",
+        "availableInventory": 5
+    }
+]
