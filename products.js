74487984d932255f1f3d35de3968a855efcfc637
Modu <35815941+hmodu@users.noreply.github.com>
2021-07-02 13:15:11 +0100

    Update products.js

--- products.js
+++ products.js
@@ -7,63 +7,63 @@ let products = [
         "availableInventory": 5
     },
     {
-        "image": "/images/che.png",
+        "image": "images/che.png",
         "subject": "Chemistry",
         "price": 35,
         "location": "Lagos",
         "availableInventory": 5
     },
     {
-        "image": "/images/bio.jpg",
+        "image": "images/bio.jpg",
         "subject": "Biology",
         "price": 38,
         "location": "Abuja",
         "availableInventory": 5
     },
     {
-        "image": "/images/psy.jpg",
+        "image": "images/psy.jpg",
         "subject": "Psychology",
         "price": 39,
         "location": "Ontario",
         "availableInventory": 5
     },
     {
-        "image": "/images/geo.png",
+        "image": "images/geo.png",
         "subject": "Geography",
         "price": 42,
         "location": "Granada",
         "availableInventory": 5
     },
     {
-        "image": "/images/zoo.jpg",
+        "image": "images/zoo.jpg",
         "subject": "Zoology",
         "price": 49,
         "location": "Conackry",
         "availableInventory": 5
     },
     {
-        "image": "/images/eng.jpg",
+        "image": "images/eng.jpg",
         "subject": "Engineering",
         "price": 38,
         "location": "Munich",
         "availableInventory": 5
     },
     {
-        "image": "/images/com.jpg",
+        "image": "images/com.jpg",
         "subject": "Computer",
         "price": 41,
         "location": "Timbuktu",
         "availableInventory": 5
     },
     {
-        "image": "/images/mat.jpg",
+        "image": "images/mat.jpg",
         "subject": "Mathematics",
         "price": 47,
         "location": "Abuja",
         "availableInventory": 5
     },
     {
-        "image": "/images/soc.png",
+        "image": "images/soc.png",
         "subject": "Sociology",
         "price": 26,
         "location": "Toronto",
