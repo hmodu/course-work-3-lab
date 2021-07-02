f714bb9ce9b345e43d71aa13a2ec2c5739d148f8
Modu <35815941+hmodu@users.noreply.github.com>
2021-07-02 13:17:47 +0100

    Update products.js

--- products.js
+++ products.js
@@ -4,69 +4,69 @@ let products = [
         "subject": "Physics",
         "price": 50,
         "location": "Geneva",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/che.png",
         "subject": "Chemistry",
         "price": 35,
         "location": "Lagos",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/bio.jpg",
         "subject": "Biology",
         "price": 38,
         "location": "Abuja",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/psy.jpg",
         "subject": "Psychology",
         "price": 39,
         "location": "Ontario",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/geo.png",
         "subject": "Geography",
         "price": 42,
         "location": "Granada",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/zoo.jpg",
         "subject": "Zoology",
         "price": 49,
         "location": "Conackry",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/eng.jpg",
         "subject": "Engineering",
         "price": 38,
         "location": "Munich",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/com.jpg",
         "subject": "Computer",
         "price": 41,
         "location": "Timbuktu",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/mat.jpg",
         "subject": "Mathematics",
         "price": 47,
         "location": "Abuja",
-        "availableInventory": 5
+        "availableInventory": 5,
     },
     {
         "image": "images/soc.png",
         "subject": "Sociology",
         "price": 26,
         "location": "Toronto",
-        "availableInventory": 5
+        "availableInventory": 5,
     }
 ]
